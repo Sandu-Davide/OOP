c4ff49479aadf0406306f848fc985f45e6c87394
Sandu Davide Filippo <sandudavidefilippo@gmail.com>
2025-03-14 10:05:38 +0100

    Update Main.java

--- Esercizi/Liste, Ricorsione/Main.java
+++ Esercizi/Liste, Ricorsione/Main.java
@@ -9,13 +9,11 @@ public class Main {
 		list.printListaRicorsivo();
 		System.out.println("Elementi: " + list.countElement());
 		System.out.println("Lunghezza: " + list.lengthString(10) + " | " + list.lengthStringRicorsivo(10));
-		System.out.println("Media: " + list.averageLengthOfString() + " | " + list.averageLengthRicorsivo());
+		System.out.println("Media: " + list.averageLengthOfString() + " | " + list.averageLengthRicorsivo() + " | " + list.averageLengthRicorsivoI());
 		System.out.println("Uguale a: " + list.equalsString("ciao") + " | " + list.equlsRicorsivo("ciao"));
 		System.out.println("Quanti uguale a: " + list.numberOfStringEquals("ciaociao") + " | "  + list.numberOfStringEqualsRicorsivo("ciaociao"));
 		System.out.println("Max: " + list.maxOfLength() + " | " + list.maxOfLengthRicorsivo());
 		System.out.println("Max tra i min: " + list.maxOfMin(16) + " | " + list.maxOfMinRicorsivo(16));
-		
-
 	}
 
 }
